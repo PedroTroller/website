6cc9e83ff34d2aa9b3885349dd0722b38bfce68f
Fabien Schurter <fabien@fabschurt.com>
2016-08-31 23:18:19 +0200

    Fix Masonry display bugs

--- app/view/theme/js/modules/cascade-layout.js
+++ app/view/theme/js/modules/cascade-layout.js
@@ -7,14 +7,16 @@
  * file that was distributed with this source code.
  */
 
-var columnWidth = 304;
-new Masonry(document.querySelector('.work-example-list'), {
-  itemSelector: '.work-example',
-  columnWidth:  columnWidth,
-  fitWidth:     true,
-});
-new Masonry(document.querySelector('.skill-list'), {
-  itemSelector: '.skill-group',
-  columnWidth:  columnWidth,
-  fitWidth:     true,
+window.addEventListener('load', function () {
+  var columnWidth = 304;
+  new Masonry(document.querySelector('.work-example-list'), {
+    itemSelector: '.work-example',
+    columnWidth:  columnWidth,
+    fitWidth:     true,
+  });
+  new Masonry(document.querySelector('.skill-list'), {
+    itemSelector: '.skill-group',
+    columnWidth:  columnWidth,
+    fitWidth:     true,
+  });
 });
