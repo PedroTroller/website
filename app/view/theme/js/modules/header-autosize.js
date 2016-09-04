5472e459fd794506b7ff2e51730166e958a15c2a
Fabien Schurter <fabien@fabschurt.com>
2016-09-04 19:27:20 +0200

    Make header font size adapt to viewport size

---
+++ app/view/theme/js/modules/header-autosize.js
@@ -0,0 +1,14 @@
+/*
+ * This file is part of the fabschurt/cv package.
+ *
+ * (c) 2016 Fabien Schurter <fabien@fabschurt.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$(function () {
+  $('section > h1, section > h2').each(function () {
+    $(this).fitText(1.0, {maxFontSize: $(this).css('font-size')});
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ var paths = {
           basePaths.vendor + '/bootstrap-sass/assets/javascripts/bootstrap/scrollspy.js',
           basePaths.vendor + '/smooth-scroll/dist/js/smooth-scroll.js',
           basePaths.vendor + '/wow/dist/wow.js',
+          basePaths.vendor + '/jquery-fittext/jquery.fittext.js',
           basePaths.src    + '/view/theme/js/modules/**/*.js',
         ],
       },
