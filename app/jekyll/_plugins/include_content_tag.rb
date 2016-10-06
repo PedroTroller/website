1547a9934a4aad2b9b69ee4f5f74c87e722b592d
Fabien Schurter <fabien@fabschurt.com>
2016-10-06 18:36:44 +0200

    Revise CS in IncludeContentTag code

--- app/jekyll/_plugins/include_content_tag.rb
+++ app/jekyll/_plugins/include_content_tag.rb
@@ -6,13 +6,12 @@
 # file that was distributed with this source code.
 
 module Jekyll
-  # Registers the `include_content` Liquid tag with Jekyll.
+  # The `include_content` Liquid tag will search for partials in the `_content`
+  # subdirectory of the include directory and render one of them, trying to load
+  # either an HTML or Markdown file in order (and raising an exception if nothing
+  # is found).
   #
-  # The tag will search for partials in the `_content` subdirectory of the
-  # include directory and render one of them, trying to load either an HTML or
-  # Markdown file in order (and raising an exception if nothing is found).
-  #
-  # Target Markdown file(s) can contain Liquid tags, they’ll be parsed.
+  # The target Markdown file(s) can contain Liquid tags, they’ll be parsed.
   class IncludeContentTag < Liquid::Tag
     @@CONTENT_SUBFOLDER = '_content'
     @@CONTENT_FORMATS   = ['html', 'md']
@@ -21,19 +20,17 @@ module Jekyll
     # @raise [IOError] If no resolvable partial file can be found
     def render(context)
       site = context.registers[:site]
-      rendered_slug = site.liquid_renderer
-        .file('*')
-        .parse(@markup)
-        .render!(context)
-        .strip
+      rendered_slug = site.liquid_renderer.file('*')
+                                          .parse(@markup)
+                                          .render!(context)
+                                          .strip
       partial_path = "#{@@CONTENT_SUBFOLDER}/#{rendered_slug}"
       @@CONTENT_FORMATS.each do |file_ext|
         full_path = site.in_source_dir("#{partial_path}.#{file_ext}")
         if File.exist?(full_path)
-          content = site.liquid_renderer
-            .file(full_path)
-            .parse(File.read(full_path, site.file_read_opts))
-            .render!(context)
+          content = site.liquid_renderer.file(full_path)
+                                        .parse(File.read(full_path, site.file_read_opts))
+                                        .render!(context)
           case file_ext
           when 'html'
             return content
