96023cea882f88e334012911f652d0c44a992537
Fabien Schurter <fabien@fabschurt.com>
2016-10-06 22:54:59 +0200

    Remove unnecessary variable existence check

--- app/jekyll/_plugins/asset_url_tag.rb
+++ app/jekyll/_plugins/asset_url_tag.rb
@@ -10,12 +10,8 @@ module Jekyll
   # the `url.assets` in `_config.yml`) to the passed relative path.
   class AssetUrlTag < Liquid::Tag
     # @see Liquid::Tag#render
-    # @raise [RuntimeError] If `url.assets` is not set in `_config.yml`
     def render(context)
       site = context.registers[:site]
-      unless site.config['url'].to_h.key?('assets')
-        fail RuntimeError, 'The asset root URL is not set in the config file.'
-      end
 
       site.baseurl + site.config['url']['assets'] + site.liquid_renderer.file('*')
                                                                         .parse(@markup)
