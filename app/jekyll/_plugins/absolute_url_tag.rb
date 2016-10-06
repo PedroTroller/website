0404cd396d117c6f71653923ee061fb76d49d9e2
Fabien Schurter <fabien@fabschurt.com>
2016-10-06 22:52:37 +0200

    Allow for CANONICAL_ROOT env var to be undefined
    The variable existence check was meant for runtime, and it causes
    incomplete builds as Jekyll fails at build time, when the variable is
    actually not defined (it's meant to be declared at runtime).

--- app/jekyll/_plugins/absolute_url_tag.rb
+++ app/jekyll/_plugins/absolute_url_tag.rb
@@ -7,15 +7,10 @@
 
 module Jekyll
   # The `absolute_url` Liquid tag will prepend a full URL prefix to the passed
-  # relative path (the `CANONICAL_ROOT` environment variable must be set,
-  # otherwise an exception will be thrown).
+  # relative path (depends on the `CANONICAL_ROOT` environment variable).
   class AbsoluteUrlTag < Liquid::Tag
     # @see Liquid::Tag#render
-    # @raise [RuntimeError] If `ENV['CANONICAL_ROOT]` is not set
     def render(context)
-      unless ENV.key?('CANONICAL_ROOT')
-        fail RuntimeError, 'The CANONICAL_ROOT environment variable must be set in order to use this Liquid tag.'
-      end
       site = context.registers[:site]
 
       ENV['CANONICAL_ROOT'] + site.baseurl + site.liquid_renderer.file('*')
