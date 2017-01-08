9b7df82f5283be95f32a541e67ff4b3925e48d40
Fabien Schurter <fabien@fabschurt.com>
2017-01-08 19:30:14 +0100

    Use a cleaner method for translation data shortcut

---
+++ app/jekyll/_plugins/variables_hook.rb
@@ -0,0 +1,10 @@
+# This file is part of the fabschurt/website package.
+#
+# (c) 2016 Fabien Schurter <fabien@fabschurt.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
+Jekyll::Hooks.register :documents, :pre_render do |document, payload|
+  payload.site['i18n'] = payload.site.data['i18n'][document.data['lang']]
+end
